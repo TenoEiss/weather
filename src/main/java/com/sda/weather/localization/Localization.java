2de0e0de1c1514ef048d2a3eddb9350c14642a62
TenoEiss <lukasz12323@gmail.com>
2020-11-21 03:41:47 -0800

    dodanie localization, przygotowanie klasy testowej,

---
+++ src/main/java/com/sda/weather/localization/Localization.java
@@ -0,0 +1,24 @@
+package com.sda.weather.localization;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@RequiredArgsConstructor
+@Entity
+public class Localization {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String cityName;
+    String region;
+    String country;
+    float geoLength;
+    float geoWidth;
+}
