96d5e8431fc9927d682b2f3a65c373ee6b77aa63
TenoEiss <lukasz12323@gmail.com>
2020-12-13 06:28:46 -0800

    minor localization improvements, frontend build, prod and dev profiles defined

---
+++ src/main/java/com/sda/weather/AuditingConfiguration.java
@@ -0,0 +1,22 @@
+package com.sda.weather;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.security.Principal;
+import java.util.Optional;
+
+@Configuration
+public class AuditingConfiguration {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return () -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            return Optional.ofNullable(authentication).map(Principal::getName);
+        };
+    }
+}
--- src/main/java/com/sda/weather/localization/LocalizationCreateService.java
+++ src/main/java/com/sda/weather/localization/LocalizationCreateService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+
 @Component
 @RequiredArgsConstructor
 public class LocalizationCreateService {
